a5a62c6d77a3eb92e7402026765d89aa7bb15820
Francisco Pérez <devfcph@gmail.com>
2024-03-07 11:17:27 -0600

    First commit

---
+++ src/main/java/com/fcph/receipts_java/ReceiptsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.fcph.receipts_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiptsJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fcph/receipts_java/ReceiptsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcph.receipts_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
