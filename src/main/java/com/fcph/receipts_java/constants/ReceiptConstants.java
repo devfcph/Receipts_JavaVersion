7dc8ac97d5310fa9163490f5a43592d59a3e39f7
Francisco Pérez <devfcph@gmail.com>
2024-03-07 11:36:11 -0600

    Added constants class

---
+++ src/main/java/com/fcph/receipts_java/constants/ReceiptConstants.java
@@ -0,0 +1,13 @@
+package com.fcph.receipts_java.constants;
+
+public class ReceiptConstants {
+    public final static Integer THREE = 3;
+    public final static Integer FIVE=  5;
+    public static final Integer SIX = 6;
+    public static final Integer TEN = 10;
+    public static final Integer TWENTYFIVE =  25;
+    public static final Integer FIFTY = 50;
+    public static final Double ONEQUARTER = 0.25;
+    public static final Double ONEFIFTH = 0.2;
+
+}
